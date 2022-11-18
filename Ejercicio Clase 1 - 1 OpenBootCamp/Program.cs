c0f552f968e975f2ded34724ac80d540aaec526b
Tomas Dangelo <tomidngl@gmail.com>
2022-11-18 12:29:58 -0300

    Ejercicios clase 1

---
+++ Ejercicio Clase 1 - 1 OpenBootCamp/Program.cs
@@ -0,0 +1,11 @@
+﻿/*
+Ejercicio 1
+
+Escribe un programa que reciba tu nombre y lo escriba por consola.
+*/
+
+Console.WriteLine("Escriba su nombre y presione Enter: ");
+string nombre = "";
+nombre = Console.ReadLine();
+
+Console.WriteLine("\nSu nombre es " + nombre);
---
+++ Ejercicio Clase 1 - 2 OpenBootcamp/Program.cs
@@ -0,0 +1,15 @@
+﻿/*
+Ejercicio 2
+
+Escribe un programa que tome la hora y la escriba por pantalla.
+*/
+
+Console.WriteLine("Escriba la hora: ");
+string hora = "";
+hora = Console.ReadLine();
+
+Console.WriteLine("Escriba los minutos: ");
+string minutos = "";
+minutos = Console.ReadLine();
+
+Console.WriteLine("\nLa hora es: " + hora + ":" + minutos);
